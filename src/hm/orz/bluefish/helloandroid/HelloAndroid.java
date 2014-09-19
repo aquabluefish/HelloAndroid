abc921584591576c9cbd37974a2c76e15b55009c
aquabluefish <aquabluefish@gmail.com>
2014-09-19 11:03:48 +0900

    first commit

---
+++ src/hm/orz/bluefish/helloandroid/HelloAndroid.java
@@ -0,0 +1,13 @@
+package hm.orz.bluefish.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloAndroid extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_main);
+	}
+}

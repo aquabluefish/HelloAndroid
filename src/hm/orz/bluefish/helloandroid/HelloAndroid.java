ff1eb5ffaf90f7ce567f2000c124c6b18fd0642c
aquabluefish <aquabluefish@gmail.com>
2014-09-19 11:17:30 +0900

    Hello Android!

--- src/hm/orz/bluefish/helloandroid/HelloAndroid.java
+++ src/hm/orz/bluefish/helloandroid/HelloAndroid.java
@@ -2,12 +2,15 @@ package hm.orz.bluefish.helloandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HelloAndroid extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Toast.makeText(this, "Hello Android!", Toast.LENGTH_SHORT).show();
+
 //		setContentView(R.layout.activity_main);
 	}
 }

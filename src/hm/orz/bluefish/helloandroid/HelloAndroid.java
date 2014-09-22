bc6a1245e86affeb14699b6dafa00acea85646cb
aquabluefish <aquabluefish@gmail.com>
2014-09-22 22:53:06 +0900

    OnClickListener追加

--- src/hm/orz/bluefish/helloandroid/HelloAndroid.java
+++ src/hm/orz/bluefish/helloandroid/HelloAndroid.java
@@ -2,9 +2,14 @@ package hm.orz.bluefish.helloandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class HelloAndroid extends Activity {
+	private EditText edit;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,14 @@ public class HelloAndroid extends Activity {
 		Toast.makeText(this, "Hello Android!", Toast.LENGTH_SHORT).show();
 
 		setContentView(R.layout.main);
+
+        edit = (EditText)this.findViewById(R.id.editText1);
+        findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Editable name = edit.getText();
+                Toast.makeText(HelloAndroid.this, "こんにちは！ "+ name +"さん", Toast.LENGTH_LONG).show();
+            }
+        });
+
 	}
 }
